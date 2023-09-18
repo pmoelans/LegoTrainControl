db49db3d3e066649b0e739473f2b72ee2ac65a7c
pmoelans <84808693+pmoelans@users.noreply.github.com>
2023-09-18 22:51:08 +0200

    updated the train control
    with shutdown and emergency stop

--- Arduino Code/LegoEngineControl.ino
+++ Arduino Code/LegoEngineControl.ino
@@ -5,18 +5,32 @@
 
 #define en1 13  //D7
 #define en2 12  //D6
+#define shutDownPin 14 //D8
+#define Led 0//D3
 
 WiFiConnector wifiConnector;
 MqttConnector mqttConnector;
 float currentPower = 0;
 int requestedPower=0;
+bool eStop;
+bool shutDown;
 long millis_pre;
 long deltaMilli=5000;
 long millisrealTimePre = 0;
 void setup() {
+
   millis_pre= millis();
   pinMode(en1, OUTPUT);
   pinMode(en2, OUTPUT);
+
+  //keep the arduino powered on
+  pinMode(shutDownPin,OUTPUT);
+  digitalWrite(shutDownPin,HIGH);
+
+  //Set the led state to indicate that the controller is live
+  pinMode(Led, OUTPUT);
+  digitalWrite(Led, HIGH);
+
   Serial.begin(115200);
   // while (!Serial) {
   //   ;  // wait for serial port to connect. Needed for native USB
@@ -27,16 +41,28 @@ void setup() {
     Serial.println("Restarting ESP now");
     ESP.restart();
   }
-
+  
   mqttConnector.connect(MqttBroker, MqttPort, ClientId);
 
-  mqttConnector.client.subscribe(Topic2Subscribe);
-  mqttConnector.client.setCallback(callback);
+  Serial.print("Subscribed to: ");
+  Serial.println(Topic2Subscribe);
+  ResubScribe();
   Serial.println("Setup-done");
 
   millisrealTimePre= millis();
 }
+void Blink()
+{
+  digitalWrite(Led,LOW);
+  delay(300);
+  digitalWrite(Led,HIGH);
+}
+void ResubScribe()
+{
+ mqttConnector.client.subscribe(Topic2Subscribe);
+  mqttConnector.client.setCallback(callback);
 
+}
 void SendStatus() {
   StaticJsonDocument<300> statusDoc;
   statusDoc["TrainId"] = ClientId;
@@ -45,10 +71,16 @@ void SendStatus() {
 
   char Message[80] = { 0 };
   serializeJson(statusDoc, Message);
-  mqttConnector.publish(StatusTopic, Message);
+  bool resubsribeNeeded=mqttConnector.publish(StatusTopic, Message);
+  if(resubsribeNeeded)
+  {
+    ResubScribe();
+  }
+
 }
 float MeasureVbat() {
-  return analogRead(A0) * 3;  //we need to find a proper bridge for the batterymonitor
+  Serial.println(analogRead(A0));
+  return ((float)map(analogRead(A0),0,1023,0,99))/10;  //we need to find a proper bridge for the batterymonitor
 }
 void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived for this train topic: ");
@@ -56,12 +88,14 @@ void callback(char* topic, byte* payload, unsigned int length) {
 
   DynamicJsonDocument doc(256);
   deserializeJson(doc, payload);
-
+  eStop= doc["EStop"];
+  Serial.println(eStop);
+  shutDown= doc["ShutDown"];
+  Serial.println(shutDown);
   requestedPower = doc["Power"];
 
-  Serial.print("Requested speed");
-  Serial.println(requestedPower);
-  
+  //Blink the status led to indicate that we have received a message
+  Blink();
 }
 void UpdatePower() 
 {
@@ -101,9 +135,29 @@ void UpdatePower()
   analogWrite(en1, 0);
   analogWrite(en2, percentage);
 }
+void EStop()
+{
+  if(eStop>0)
+  {
+   // Serial.println("Estop");
+    currentPower = 0;
+    requestedPower = 0;
+    analogWrite(en1, 0);
+    analogWrite(en2, 0);
+  }
+}
+
+void PowerDown()
+{
+  if(shutDown>0)
+  {
+    //Serial.println("PowerDown");
+    digitalWrite(shutDownPin,LOW);
+  }
+}
 
 void loop() {
- mqttConnector.client.loop();
+  mqttConnector.client.loop();
   // put your main code here, to run repeatedly:
   if((millis()-millis_pre)>deltaMilli)
   {
@@ -111,7 +165,14 @@ void loop() {
       millis_pre = millis();
   }
 
-  UpdatePower();
+  //Check if we need to perform an emergency brake
+  EStop();
+
+  //Check if we need to perform a power down
+  PowerDown();
+
+  //Update the power settings
+  UpdatePower();  
+
   millisrealTimePre=millis();
-  //delay(5000);
 }

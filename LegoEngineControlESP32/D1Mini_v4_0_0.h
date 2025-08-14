bcfab598cd807c9d527a25337bc14445997b860f
pmoelans <84808693+pmoelans@users.noreply.github.com>
2025-08-14 21:07:56 +0200

    Delete LegoEngineControlESP32 directory

--- LegoEngineControlESP32/D1Mini_v4_0_0.h
+++
@@ -1,9 +0,0 @@
-#define en1A 14   //D1
-#define en2A 12   
-#define en12 16   
-#define en3A 0  //D6
-#define en4A 4 //D7
-#define en34 2  //D8
-#define BtnPress 13//D0
-#define shutDownPin  5 //D5
-#define Led        15   //D4

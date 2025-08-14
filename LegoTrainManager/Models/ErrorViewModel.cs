c48350ae86aaadc610b6c2875be9eb31969aa20e
pmoelans <84808693+pmoelans@users.noreply.github.com>
2025-08-14 21:05:32 +0200

    Delete LegoTrainManager directory

--- LegoTrainManager/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace LegoTrainManager.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
